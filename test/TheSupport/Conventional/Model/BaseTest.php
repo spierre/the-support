4f6da85a00e2b9ba2e43807cb025ce43b274026e
pawlik <grzegorzpawlik@gmail.com>
2013-05-31 16:53:14 +0200

    restore accidentaly deleted tests

--- test/TheSupport/Conventional/Model/BaseTest.php
+++ test/TheSupport/Conventional/Model/BaseTest.php
@@ -1,5 +1,5 @@
 <?php
-namespace TheSupport\Test\Conventional\Model\Base\Base;
+namespace TheSupport\Test\Conventional\Model;
 
 use TheSupport\Conventional\Model\Base;
 
@@ -8,16 +8,35 @@ class BaseTest extends \PHPUnit_Framework_TestCase {
     public function test_it_should_allow_initialization_with_empty_fields()
     {
         $model = new DummyModel();
-        $this->assertEquals("overrided", $model->field);
+        $this->assertEquals(null, $model->field);
     }
+
+    public function test_it_should_allow_accessing_fields_by_getter_if_undefined()
+    {
+        $model = new DummyModel(array("field" => 23));
+
+        $this->assertEquals(23, $model->getField());
+
+    }
+
+    public function test_to_array_nullify_empty_fields_by_default()
+    {
+        $model = new DummyModel(array('field' => '', 'field2' => ""));
+        $data = $model->toArray();
+        $this->assertFalse(isset($data['field']));
+        $this->assertFalse(isset($data['field2']));
+    }
+
+    public function test_toArray_allow_disabling_nullification()
+    {
+        $model = new DummyModel(array('field' => ''));
+        $data = $model->toArray(false);
+        $this->assertTrue($data['field'] === '');
+    }
+
 
 }
 
 class DummyModel extends Base {
     protected $attrs = array("field", 'field2', 'id');
-
-    public function getField()
-    {
-        return "overrided";
-    }
 }
